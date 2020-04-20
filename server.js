4293d6f16c4f147fe86b6a8fe6e5c0c89dd1001e
conan2613-dev <githubprojects2613@gmail.com>
2020-04-20 15:12:12 +0530

    initail author

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 const indexRouter = require('./routes/index')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const authorRouter = require('./routes/authors')
 
 //MLABS MONGO DB
 mongoose.connect('mongodb+srv://sagar:sagar12345@myfirstmongodb-tldsv.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 const db = mongoose.connection;
 db.on('error', (error) => {
@@ -24,7 +29,10 @@ app.set('views', './views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayout);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+
 
 app.listen(process.env.PORT || 3000);
